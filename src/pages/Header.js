d599347bb44dba601e7df2865ad07be25a3f0a42
henrique <hickm018@gmail.com>
2022-02-15 01:24:04 -0300

    Finaliza cards de musica

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -25,18 +25,18 @@ class Header extends React.Component {
         {isLoading
           ? <Loading />
           : (
-            <div className="flex flex-col flex-wrap items-center bg-blue-750">
-              <div className="flex flex-row bg-blue-800 px-5 py-10 w-full justify-around items-center">
+            <div className="flex flex-col flex-wrap items-center bg-gradient-to-r from-blue-800 via-white to-blue-800">
+              <div className="flex flex-row px-5 py-10 w-full justify-around items-center">
                 <img src={ logo } alt="all-music" className="w-28"/>
                 <h1 className="text-5xl font-bold">TrybeTunes</h1>
-                  <Link to="/profile" className="flex flex-row w-36 justify-between items-center bg-indigo-50 rounded-xl px-3 py-2" >
+                  <Link to="/profile" className="flex flex-row w-36 justify-between items-center bg-indigo-50 hover:bg-indigo-100 rounded-xl px-3 py-2" >
                     <ImUser className="w-auto" />
                     <p className="text-xl font-sans font-semibold mt-1">{ name }</p>
                   </Link>
               </div>
               <nav className="font-bold text-xl flex flex-row px-8 py-8 w-1/2 justify-between items-center">
-                <Link to="/search" >Pesquisar</Link>
-                <Link to="/favorites" >Favoritos</Link>
+                <Link to="/search" className="hover:bg-gradient-to-r from-blue-300 to-blue-50 cursor-pointer rounded-xl py-3 px-12 text-2xl" >Pesquisar</Link>
+                <Link to="/favorites" className="hover:bg-gradient-to-r from-blue-50 to-blue-300 cursor-pointer rounded-xl py-3 px-12 text-2xl" >Favoritos</Link>
               </nav>
             </div>
           )}
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,11 @@ module.exports = {
         '3xl': '0 35px 60px -15px black',
       },
       colors: {
-        'primary-1': '#243c5a',
+        'gradient': 'linear-gradient(180deg, #075985, #165c88, #2e668f, #48759a, #6487a9, #829db8, #a0b3c9, #bccad9, #d6dee8, #eceff4, #fafbfc, #ffffff);',
       },
+      maxHeight: {
+        '128': '32rem',
+      }
     },
   },
   plugins: [],
